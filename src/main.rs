46a1f21e74dc73af8c127fb080a2d02113ad1607
hedgar2017 <hedgar2017@gmail.com>
2019-02-03 21:51:20 +0200

    Publish version

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::{
     io::{self, BufRead},
 };
 
-use patch_rs::{PatchProcessor, PatchResult};
+use patch_rs::{PatchProcessor, PatchError};
 
 fn read_to_vec(path: &str) -> io::Result<Vec<String>> {
     let file = fs::File::open(path)?;
@@ -15,7 +15,13 @@ fn read_to_vec(path: &str) -> io::Result<Vec<String>> {
     file.lines().collect()
 }
 
-fn main() -> PatchResult<()> {
+#[derive(Debug)]
+enum Error {
+    Reading(io::Error),
+    Patch(PatchError),
+}
+
+fn main() -> Result<(), Error> {
     env_logger::Builder::from_default_env()
         .default_format_timestamp_nanos(true)
         .init();
@@ -45,11 +51,11 @@ fn main() -> PatchResult<()> {
     let file = args.value_of("file").expect("Unreachable");
     let patch = args.value_of("patch").expect("Unreachable");
 
-    let text = read_to_vec(file)?;
-    let patch = fs::read_to_string(patch)?;
+    let text = read_to_vec(file).map_err(Error::Reading)?;
+    let patch = fs::read_to_string(patch).map_err(Error::Reading)?;
 
-    let parser = PatchProcessor::converted(text, &patch)?;
-    for s in parser.process()? {
+    let parser = PatchProcessor::converted(text, &patch).map_err(Error::Patch)?;
+    for s in parser.process().map_err(Error::Patch)? {
         println!("{}", s);
     }
 
